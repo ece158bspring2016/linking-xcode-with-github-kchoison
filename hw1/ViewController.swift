19903710a4d7db804e570f55046da99c302ab8ae
Keith Choison <mickeychoison@sbcglobal.net>
2016-04-06 15:23:43 -0700

    Initial Commit

---
+++ hw1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hw1
+//
+//  Created by Keith Choison on 4/6/16.
+//  Copyright © 2016 kchoison. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
